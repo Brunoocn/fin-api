9d0c54de24f41a145cda4c7c4c508b6cfd88f748
Bruno Caneo <brunocaneo3@gmail.com>
2022-03-29 16:14:47 -0300

    adding new features

--- src/index.js
+++ src/index.js
@@ -5,20 +5,37 @@ const app = express();
 
 app.use(express.json());
 
-const custumers = [];
+const customers = [];
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
 
-  custumers.push({
+  costumerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if (costumerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
     name,
     cpf,
-    id,
+    id: uuidv4(),
     statement: [],
   });
 
   return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+
+  const custumer = customers.find((costumer) => costumer.cpf === cpf);
+
+  if (!custumer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+
+  return response.json(custumer.statement);
 });
 
 app.listen(3333);
