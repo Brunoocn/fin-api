a78383cd15d9f8b9c168d287035b4de3088b2261
Bruno Caneo <brunocaneo3@gmail.com>
2022-03-30 02:05:48 -0300

    Adding middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const custumer = customers.find((costumer) => costumer.cpf === cpf);
+
+  if (!custumer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -26,15 +39,8 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const custumer = customers.find((costumer) => costumer.cpf === cpf);
-
-  if (!custumer) {
-    return response.status(400).json({ error: "Customer not found!" });
-  }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(custumer.statement);
 });
 
